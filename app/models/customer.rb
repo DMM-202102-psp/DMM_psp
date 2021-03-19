0dc066f942112c1a7f2d5d721f21c2d2f97c90dd
yuuichi-komaki <yuuichi.k.1109@gmail.com>
2021-03-19 05:17:34 +0000

    カスタマーのバリデーション追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,5 +7,20 @@ class Customer < ApplicationRecord
   has_many :carts
   has_many :orders
   has_many :addresses
-
+  
+  # 名前（姓）のバリデーション
+   validates :last_name, presence: true, format: { with:/\A[一-龥ぁ-ん]/ }
+  #名前（名）のバリデーション
+   validates :last_name, presence: true, format: { with:/\A[一-龥ぁ-ん]/ }
+  #フリガナ（セイ）のバリデーション
+   validates :last_name_kana, presence: true, format: { with:/\A[ァ-ヶー－]+\z/ }
+  #フリガナ（メイ）のバリデーション
+   validates :last_name_kana, presence: true, format: { with:/\A[ァ-ヶー－]+\z/ }
+  #電話番号（ハイフンなし）のバリデーション
+   validates :phone_number, presence: true, format: { with:/\A\d{10,11}\z/ }
+  #住所のバリデーション
+   validates :addresses, presence: true
+  # 郵便番号（ハイフンなし）のバリデーション
+   validates :postcode, presence: true, format: { with:/\A\d{7}\z/ }
+  
 end
