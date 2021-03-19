1f62035d715e44c3c2492a50a36735c461ceeae8
shintarokabe <kabe0852@gmail.com>
2021-03-19 10:37:35 +0000

    update_add_admin_genres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,22 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genres = Genre.all
   end
 
   def create
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
   end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
