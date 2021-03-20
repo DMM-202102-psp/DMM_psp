e2ae8b8654fd090960e65fd230fd49aea11d668d
yuuichi-komaki <yuuichi.k.1109@gmail.com>
2021-03-20 02:53:26 +0000

    管理者がわ会員編集の追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,11 +6,21 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def update
+     @customer = Customer.find(params[:id])
+     @customer.update(customer_params)
+     redirect_back(fallback_location: root_url)
   end
 end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:postcode, :email, :encrypted_password, :address, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :is_valid)
+  end
