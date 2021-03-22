e09221e88d9f6fe3390d3ccea252e0b872b3cac0
Katsumasa-Toda <katsumasatoda7@gmail.com>
2021-03-22 02:33:17 +0000

    order_info_input

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,7 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    params.require(:customer).permit(:postcode, :address, :last_name, :first_name)
   end
 
   def confirm
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   has_many :order_items
   belongs_to :customer
+  enum term: { クレジットカード: 1, 銀行振込: 2}
 end
