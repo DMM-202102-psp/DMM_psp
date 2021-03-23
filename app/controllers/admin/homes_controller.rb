94fc0217e300c3c74dcbd1197cb35ee39918e744
Katsumasa-Toda <katsumasatoda7@gmail.com>
2021-03-23 11:34:21 +0000

    admin_order_index

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
-    @items = Item.page(params[:page]).per(10)
+    @customers = Customer.all
+    # @customer = Customer.find(params[:id])
   end
 end
