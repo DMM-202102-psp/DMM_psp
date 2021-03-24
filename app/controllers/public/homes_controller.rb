b7bf6d502420c6c3a19cc790ce8e1044f2f4b84f
furuya-tomoki <furuya11221993@gmeil.com>
2021-03-24 04:55:38 +0000

    test

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,14 @@
 class Public::HomesController < ApplicationController
   def top
+     @items = Item.all
   end
 
   def about
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image_id, :name, :description, :genre_id, :price, :is_active)
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,9 +1,4 @@
 class Genre < ApplicationRecord
   has_many :items
 
-<<<<<<< HEAD
 end
-=======
-end
-
->>>>>>> 7fc6048c58b1cecf3b06b5405f45d6da660738c7
