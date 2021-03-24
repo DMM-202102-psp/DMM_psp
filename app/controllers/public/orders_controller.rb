17f693bd627f5b2d190dbd58bed6bafa7c09fdbf
Katsumasa-Toda <katsumasatoda7@gmail.com>
2021-03-24 04:27:20 +0000

    order_info_confirm2

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,17 +3,39 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @address = Address.new
     @addresses = Address.all
+    @carts = current_customer
     # @custmer = current_customer
   #   params.require(:customer).permit(:postcode, :address, :last_name, :first_name)
   end
 
   def confirm
     @order = Order.new(order_params)
+    if params[:order][:address] == "myaddress"
+      @order.postcode = current_customer.postcode
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address] == "entryaddress"
+      @address = Address.find(params[:order][:address_id])
+      @order.postcode = @address.postcode
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:address] == "addaddress"
+      @addressnew = Address.new
+      # @addressnew = Address.find(params[:order][:address_id])
+      @order.postcode = @addressnew.postcode
+      @order.address = @addressnew.address
+      @order.name = @addressnew.name
+    end
   end
 
   def create
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
     @order.save
+    @carts
+    #カートの中身をorder_detailに保存する作業を入れる
+    #カートの中身を削除する作業が必要
+    #終わった後のリダイレクト先を→完了画面
   end
 
   def complete
@@ -30,5 +52,10 @@ class Public::OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:postcode, :address, :name, :carriage, :term, :total_price, :status)
     end
+
+    def set address
+      @address = Address.find(params[:id])
+    end
+
 
 end
