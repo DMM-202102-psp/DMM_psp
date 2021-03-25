405b5578d26d75befec00438967ee9c62c4dba47
furuya-tomoki <furuya1122@gmail.com>
2021-03-25 08:43:42 +0000

    [Add]items_new

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,7 @@ class Admin::HomesController < ApplicationController
   def top
     @customers = Customer.all
     # @customer = Customer.find(params[:id])
+    @items = Item.all
+     @items = Item.page(params[:page]).per(10)
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :customer
   belongs_to :item
-  
-  def total
-   ((self.item.price * 1.1).floor * self.quantity )
-  end 
-  
+
 end
